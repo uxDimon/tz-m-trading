6ba4e4758ac3dd19fc07d2a652b1923eeea808de
Dmitry Shelkov <bobyorrr@gmail.com>
2022-07-12 23:53:38 +0300

    Рандомные 4 ключа для json

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,54 @@
 <script>
-	// import logo from "./assets/svelte.png";
-	import Counter from "./lib/Counter.svelte";
+	import "./App.scss";
 	import Btn from "./components/Btn/Btn.svelte";
 	import TraderBtn from "./components/TraderBtn/TraderBtn.svelte";
 
+	import json from "./input.json";
+
+	// Рандомные 4 ключа для json
+	const randomKey = [];
+
+	const getRandom = (max) => {
+			return Math.floor(Math.random() * (max + 1));
+		},
+		pushRandomNumber = (max, keyList) => {
+			// если max будет меньше чем итераций то будет ошибочка(
+			const randomNumber = getRandom(max);
+
+			const isRepeat = keyList.every((item) => {
+				return randomNumber !== item;
+			});
+
+			if (isRepeat) {
+				keyList.push(randomNumber);
+			} else {
+				pushRandomNumber(max, keyList);
+			}
+		};
+
+	for (let index = 0; index < 4; index++) {
+		pushRandomNumber(json.length, randomKey);
+	}
+
+	console.log(randomKey);
+
+	//
+	const traderList = [];
+
 	const btn = {
 		index: 1,
-		monthlyProfit: 233.23,
+		monthlyProfit: -233.23,
 		name: "Crystal Williams",
 		img: "https://flagcdn.com/h40/in.png",
-		isActive: true,
+		isActive: false,
 	};
 </script>
 
-<main>
-	<!-- <img src={logo} alt="Svelte Logo" /> -->
-
-	<Counter />
-	<TraderBtn {...btn} />
+<main class="app">
+	<h1 class="app__title">Copy the best masters</h1>
+	<ul class="app__trader-list">
+		<li class="app__trader-list__item">
+			<TraderBtn {...btn} className="app__trader-list__btn" />
+		</li>
+	</ul>
 </main>
--- src/components/TraderBtn/TraderBtn.svelte
+++ src/components/TraderBtn/TraderBtn.svelte
@@ -2,10 +2,11 @@
 	import "./TraderBtn.scss";
 
 	export let index = 0,
+		isActive = false,
 		name = "Name unknown",
 		monthlyProfit = null,
 		img = null,
-		isActive = false,
+		className = null,
 		onClick = () => {
 			return false;
 		};
@@ -21,11 +22,9 @@
 	} else {
 		monthlyProfit = "+ " + monthlyProfit + "%";
 	}
-
-	if (isActive) profitClassName + " _active";
 </script>
 
-<button class="trader-btn" title={name} on:click={onClick}>
+<button class="trader-btn {className}" class:_active={isActive} title={name} on:click={onClick}>
 	<img src={img} alt={name} class="trader-btn__img" />
 	<div class="trader-btn__wrap">
 		<span class="trader-btn__name">{index + ". " + name}</span>
